2673ecb98ed12cd95633f4360c047a74cd44522b
zz w <1363594155@qq.com>
2025-11-27 16:37:47 +0800

    Add files via upload

---
+++ 计算机组成原理/lab/lab3/lab3.srcs/sources_1/new/TOP.v
@@ -0,0 +1,50 @@
+`timescale 1ns / 1ps
+
+
+module TOP (
+    rst,
+    clk,
+    clk_A,
+    clk_B,
+    clk_F,
+    data,
+    leds,
+    which,
+    seg
+);
+
+  input rst, clk, clk_A, clk_B, clk_F; 
+  input [31:0] data;
+
+  output [3:0] leds;
+
+  output [3:0] which;
+
+  output [7:0] seg;
+
+  wire [31:0] A, B, F;
+
+
+  ALU_REG u1 (
+      data[3:0],
+      data,
+      data,
+      rst,
+      clk_A,
+      clk_B,
+      clk_F,
+      A,
+      B,
+      F,
+      leds
+  );
+
+  DISPLAY u2 (
+      clk,
+      F,
+      which,
+      seg
+  );
+
+
+endmodule
